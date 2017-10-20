dbcfa89a21991fc7a1adc98fba034124272e1753
Tree <tree@multiplexed.be>
2017-10-20 13:41:05 -0700

    setting up got screen size

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-var app = require('app')
-var bowser = require('browser-window')
-var Menu = require('menu')
-var MenuItem = require('menu-item')
+var fs = require('fs')
+var electron = require('electron')
+var app = electron.app
+var bowser = electron.BrowserWindow 
+var Menu = electron.Menu
+var MenuItem = electron.MenuItem
+var ipc = electron.ipcMain 
+var window, screenSize = []
+
+ipc.on('screenxy', (evt, val) => {
+  screenSize = val
+  console.log(val)  
+}) 
 
 app.on('ready', function(){
-  var window = new bowser({
-    width: 640,
-    height: 480
+  let screen = new bowser({
+    show: false,
+    backgroundColor: 'transparent'
   })
-  window.loadUrl('file://' + __dirname + '/public/index.html')
+  screen.setFullScreen(true)
+  screen.hide()
+  screen.loadURL('file://'+__dirname +'/public/screenxy.html')
+
+  window = new bowser({
+    width: 1080,
+    height: 720,
+    x: 0,
+    y: 0,
+    //fullscreen: true,
+    show: false,
+    frame: false
+  })
+  window.loadURL('file://' + __dirname + '/public/index.html')
   window.openDevTools()
+  window.show()
   var menu = Menu.buildFromTemplate([{
     label: 'options',
     submenu: [{
